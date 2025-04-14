eaf163b34f13fe9b27f305cdacfe04d7a060197a
Varun Sharma <varunsh056@gmail.com>
2025-04-14 08:00:03 +0530

    add the Firstcode.java

---
+++ Firstcode.java
@@ -0,0 +1,48 @@
+package practicee;
+import java.util.*;
+
+
+class Rectangele{
+    int length;
+    int breadth;
+
+    Rectangele(){
+        length=breadth=1;
+
+    }
+    Rectangele(int l,int b){
+        length=l;
+        breadth=b;
+    }
+}
+
+class Cuboid extends Rectangele{
+    int height;
+    Cuboid()
+    {
+        height=1;
+    }
+    Cuboid(int h){
+        height=h;
+
+    }
+    Cuboid(int l,int b,int h){
+        super(l,b);
+        height=h;
+    }
+    int volume(){
+        return length*breadth*height;
+    }
+}
+
+public class Firstcode {
+
+    
+    public static void main(String arg[])
+    {
+        Cuboid c=new Cuboid(5,3,10);
+        System.out.println(c.volume());
+
+
+    }
+}
